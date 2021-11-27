91736fd74b413c010ec6656893818933154b2376
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2021-11-27 13:32:42 +0300

    feat: use dash for naming classes (point 3.1)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var taskInput = document.getElementById("new-task");
 var addButton = document.getElementsByTagName("button")[0];
-var incompleteTaskHolder = document.getElementById("incompleteTasks");
+var incompleteTaskHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("completed-tasks");
 
 
@@ -51,7 +51,7 @@ var editTask = function () {
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("editMode");
+  var containsClass = listItem.classList.contains("edit-mode");
   if (containsClass) {
     label.innerText = editInput.value;
     editBtn.innerText = "Edit";
@@ -59,7 +59,7 @@ var editTask = function () {
     editInput.value = label.innerText;
     editBtn.innerText = "Save";
   }
-  listItem.classList.toggle("editMode");
+  listItem.classList.toggle("edit-mode");
 }
 
 var deleteTask = function () {
